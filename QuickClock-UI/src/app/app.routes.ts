e03ecafc7d7a01f479aab103c5497b2f3709a123
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-12-03 21:38:41 +0100

    setup auth routes

--- QuickClock-UI/src/app/app.routes.ts
+++ QuickClock-UI/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { MainLayoutComponent } from './layout/main-layout.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: MainLayoutComponent },
+    {
+        path: 'dashboard',
+        component: MainLayoutComponent,
+        canActivate: [authGuard],
+    },
 ];
