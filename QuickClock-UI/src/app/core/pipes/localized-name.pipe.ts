d4cdfdf7a95bb15246bc7d85ffe6916ef5b99487
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-12-28 03:54:13 +0100

    add User type to localized name pipe and use first name only

--- QuickClock-UI/src/app/core/pipes/localized-name.pipe.ts
+++ QuickClock-UI/src/app/core/pipes/localized-name.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform, Inject, LOCALE_ID } from '@angular/core';
 import { UserSummaryDTO } from '../models/dto/user-summary-dto.model';
+import { User } from '../models/user.model';
 
 @Pipe({
     name: 'localizedName',
@@ -8,15 +9,14 @@ import { UserSummaryDTO } from '../models/dto/user-summary-dto.model';
 export class LocalizedNamePipe implements PipeTransform {
     constructor(@Inject(LOCALE_ID) private locale: string) {}
 
-    transform(user: UserSummaryDTO | null | undefined): string {
+    transform(user: User | UserSummaryDTO | null | undefined): string {
         if (!user) {
             return '';
         }
 
         if (this.locale.startsWith('ar') && user.displayNameArabic) {
-            console.log('Using Arabic name:', user.displayNameArabic);
-            return user.displayNameArabic;
+            return user.displayNameArabic.split(' ')[0]; // Return first name in Arabic
         }
-        return user.displayName;
+        return user.displayName.split(' ')[0];
     }
 }
